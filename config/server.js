f90671352d4b43907d7af01e610a28ab1126b43f
Askar Karasaev <askar.karasaev@gmail.com>
2021-07-25 16:47:46 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b5441fee2a235f09c72bcdf4b42fe4a0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ff85ad5f-5ab4-4c88-bfa9-c954dca1834b'
+};
