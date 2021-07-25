67d9bfca374bbf06e83300ca6bf8b46f77901e17
Askar Karasaev <askar.karasaev@gmail.com>
2021-07-25 17:08:15 +0900

    Adding remote db

--- config/database.js
+++ config/database.js
@@ -5,12 +5,14 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'kaganat-strapi'),
-        username: env('DATABASE_USERNAME', 'askar'),
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        },
       },
       options: {}
     },
